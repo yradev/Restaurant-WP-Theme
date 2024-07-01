4192cad075a6bbfbe8e66b0157fd256cd28a718f
Yanko Radev <yanko.radev1995@gmail.com>
2024-07-01 16:04:35 +0300

    Add typographics

--- themes/WP-Custom-Theme/functions.php
+++ themes/WP-Custom-Theme/functions.php
@@ -1,9 +1,7 @@
 <?php
-
 include_once( __DIR__ . '/includes/variables.php' );
 include_once( __DIR__ . '/includes/enqueues.php' );
 include_once( __DIR__ . '/includes/custom-pages.php' );
 include_once( __DIR__ . '/includes/helpers.php' );
 include_once( __DIR__ . '/includes/woocommerce.php' );
 include_once( __DIR__ . '/includes/menus.php' );
-
