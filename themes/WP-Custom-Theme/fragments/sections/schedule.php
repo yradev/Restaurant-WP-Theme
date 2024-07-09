ff691b4082f0861a34bf57ca72066b6add111fb9
Yanko Radev <yanko.radev1995@gmail.com>
2024-07-09 17:52:20 +0300

    Add section-schedule -- responsive

---
+++ themes/WP-Custom-Theme/fragments/sections/schedule.php
@@ -0,0 +1,49 @@
+<section class="section-schedule">
+	<div class="shell">
+		<div class="section__inner">
+			<div class="section__content">
+				<?php if( ! empty( $title ) ) :?>
+					<h2><?php echo $title ?></h2>
+				<?php endif ?>	
+
+				<?php if( ! empty( $schedule ) ) :?>
+					<ul>
+						<?php foreach( $schedule as $day ) :?>
+							<li>
+								<h3><?php echo $day['day'] ?></h3>
+
+								<?php if( $day['is_open'] ) :?>
+									<p>: <?php echo $day['open_time'] ?> - <?php echo $day['close_time'] ?></p>
+								<?php else: ?>
+									<p><?php _e('Closed' , 'crb') ?></p>
+								<?php endif ?>
+							</li>
+						<?php endforeach ?>
+					</ul>
+				<?php endif ?>
+			</div><!-- /.section__content -->
+		
+			<div class="section__images">
+				<div class="section__two-images">
+						<?php 
+							if( ! empty( $images ) ) {
+								echo wp_get_attachment_image( $images[0]['image'] , 'full' );
+							}
+
+							if ( ! empty( $images[1] ) ) {
+								echo wp_get_attachment_image( $images[1]['image'] , 'full' );
+							}
+						?>				
+					</div><!-- /.section__two-images -->
+
+					<div class="section__image">
+						<?php 
+							if ( ! empty( $images[2] ) ) {
+								echo wp_get_attachment_image( $images[2]['image'] , 'full' );
+							}
+						?>		
+					</div><!-- /.section__image -->
+			</div><!-- /.section__images -->
+		</div><!-- /.section__inner -->
+	</div><!-- /.shell -->
+</section><!-- /.section-working-schedule -->
