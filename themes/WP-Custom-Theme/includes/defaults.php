74c10a25177cd68f8bc06be3d5818e52f71cfa6a
Yanko Radev <yanko.radev1995@gmail.com>
2024-08-26 11:33:27 +0300

    Add redirection to https

--- themes/WP-Custom-Theme/includes/defaults.php
+++ themes/WP-Custom-Theme/includes/defaults.php
@@ -1,5 +1,18 @@
 <?php
+
 add_action( 'init', 'ct_add_theme_support', 0 );
 function ct_add_theme_support() {
 	add_theme_support('post-thumbnails');
 }
+
+/**
+ * Force redirection to https
+ */
+
+ add_action ( 'template_redirect' , 'ct_domain_redirect' , 999 );
+ function ct_domain_redirect() {
+	if( ! is_ssl() || str_starts_with($_SERVER['HTTP_HOST'], 'www.')) {
+		wp_redirect('https://' . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'], 301);
+		exit();
+	}
+ }
