a3414ba129a08d2a57c9164a1e9ad6fae6689b62
Yanko Radev <yanko.radev1995@gmail.com>
2024-08-26 12:14:30 +0300

    Add alt to images where doesnt have alt text

--- themes/WP-Custom-Theme/functions.php
+++ themes/WP-Custom-Theme/functions.php
@@ -1,5 +1,6 @@
 <?php
 include_once( __DIR__ . '/includes/defaults.php' );
+include_once( __DIR__ . '/includes/SEO.php' );
 include_once( __DIR__ . '/includes/post-types.php' );
 include_once( __DIR__ . '/includes/taxonomies.php' );
 include_once( __DIR__ . '/includes/variables.php' );
---
+++ themes/WP-Custom-Theme/includes/SEO.php
@@ -0,0 +1,31 @@
+<?php 
+
+/**
+ * Force redirection to https
+ */
+
+//  add_action ( 'template_redirect' , 'ct_domain_redirect' , 999 );
+//  function ct_domain_redirect() {
+// 	if( ! is_ssl() || str_starts_with($_SERVER['HTTP_HOST'], 'www.')) {
+// 		wp_redirect('https://' . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'], 301);
+// 		exit();
+// 	}
+//  }
+
+/**
+ * Add description to images if the image doesnt have description.
+ */
+
+ add_filter('wp_get_attachment_image_attributes' , 'ct_add_description_to_image' , 10 , 5);
+	function ct_add_description_to_image( $attr ){
+		if( empty( $attr['alt'] ) ) {
+			$src_array = explode('/' , $attr['src']);
+			$file_name = end($src_array);
+			$base_name = explode( '.' , $file_name)[0];
+			$alt_text = str_replace('-' , ' ', $base_name);
+
+			$attr['alt'] = $alt_text;
+		}
+
+	return $attr;
+ } 
